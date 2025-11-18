83364fd7b0f0644dfd56f21e8ad85a285b333a91
Nzrr <god.nizaryt10@gmail.com>
2025-11-18 18:14:47 +0100

    Fixed testPage Path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from './Components/Navbar';
-import LongTestPage from './pages/testPage'
+import LongTestPage from '/src/Pages/testPage.jsx'
 
 export default function App() {
   return (
